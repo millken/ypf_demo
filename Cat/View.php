2a16f07c001d1f7dfc3cf79bc8e67aab4d22fbd5
millken <millken@gmail.com>
2016-04-22 15:02:13 +0800

    update

--- Cat/View.php
+++ Cat/View.php
@@ -7,14 +7,14 @@ class View extends \Ypf\Core\View {
 	private $response;
 
 	public function setResponse($response) {
-		$this->response = $response;
+		$this->response = &$response;
 	}
 
 	public function display($template) {
 		$output = $this->fetch($template);
-		if(defined("__MODE__") && __MODE__ == "swoole") {
-			$this->response->setOutput($output);
-		}else{
+		if (defined("__MODE__") && __MODE__ == "swoole") {
+			\Ypf\Swoole\Response::getInstance()->setOutput($output);
+		} else {
 			echo $output;
 		}
 	}
