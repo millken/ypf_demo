46f1284b61af2812412ee70b17abf0c81f1cbaf5
millken <millken@gmail.com>
2018-09-16 23:00:46 +0800

    save work

--- swoole.php
+++ swoole.php
@@ -2,6 +2,17 @@
 
 require './vendor/autoload.php';
 require '../ypf/vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/hello/{name}', function ($request) {
+        //The route parameters are stored as attributes
+        $name = $request->getAttribute('name');
+
+        //You can echo the output (it will be captured and written into the body)
+        return sprintf('Hello %s', str_repeat($name, mt_rand(100, 10000)));
+    });
+});
+
 $services = [
     'factory' => Ypf\Application\Swoole::class,
 
@@ -9,7 +20,9 @@ $services = [
         'listen' => '*:7000',
     ],
     'middleware' => [
-        Middleware\BenchmarkMiddleware::class,
+        new Middleware\BenchmarkMiddleware(),
+        //new Middlewares\FastRoute($dispatcher),
+        new Middlewares\RequestHandler(),
         Middleware\RewriteMiddleware::class,
     ],
     //'dispatcher' => Middleland\Dispatcher::class;
